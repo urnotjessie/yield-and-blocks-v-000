ed0906e292957db242292aae829e11e53b6f3f07
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-01 17:30:33 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -9,12 +9,7 @@ def hello_t(array)
     else
         puts "Hey! No block was given!"
     end
-
 end
 
 # call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
+
